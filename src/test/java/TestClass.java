0fa16c432e2995de966974b01af262c61b54a5d0
Bogdan <Bogdan346@i.ua>
2019-04-18 12:24:28 +0300

    Add waiters for elements

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -2,26 +2,34 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestClass {
 
     WebDriver driver = WebDriverSetting.getInstance();
-
+    WebDriverWait wait = new WebDriverWait(driver, 5);
 
     @Test
     public void testBBCPage() {
         driver.get("https://bbc.com");
         WebElement searchField = driver.findElement(By.id("orb-search-q"));
-        System.out.println(searchField.isDisplayed());
+        waiterDisplaed(searchField);
+
         searchField.sendKeys("people");
+
         WebElement searchButton = driver.findElement(By.xpath("//*[@id='orb-search-button']"));
-        System.out.println(searchButton.isEnabled());
+        waiterDisplaed(searchButton);
         searchButton.click();
-        driver.findElement(By.xpath("//*[@data-result-number='1']//h1//a"));
+        driver.findElement(By.xpath("//*[@data-result-number='1']//h1//a")).click();
         //driver.close();
     }
 
 
+    public void waiterDisplaed(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+
+    }
 }
 
 
